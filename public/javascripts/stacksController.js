d4bf2bfa207c0439aa9256400ff28fb5d9e4fd2f
Kelli Waiss <kelliwaiss@hotmail.com>
2016-03-02 21:01:53 -0600

    able to show one term and definition on card

--- public/javascripts/stacksController.js
+++ public/javascripts/stacksController.js
@@ -4,8 +4,11 @@
 
   app.controller('StacksController', function($http) {
     this.newCard = { term: '', definition: '' };
-    
+    this.currentCard = { term: '', definition: '' };
+    this.index = 0;
+
     var self = this;
+    
 
     $http({
       method: 'GET',
@@ -14,26 +17,39 @@
       console.log('success', response.data);
 
       self.stacks = response.data;
+
+      console.log(response.data);
+
+      self.currentCard = {
+        term: response.data[self.index]['cards'][0].term,
+        definition: response.data[self.index]['cards'][0].definition
+      };
+      console.log('got here');
       
       // angular.forEach(response.data, function(value, key){
       //   console.log(key + ' : ' + value)      
       // })
 
-      return this;
+      return self;
 
 
     }, function errorCallback(response) {
 
     });
-    
+
 
     var counter = 0;
 
     this.nextCard = function() {
-      counter++;
+      self.index++;
 
-      index++;
+      self.currentCard = {
+        term: self.stacks[self.index]['cards'][0].term,
+        definition: self.stacks[self.index]['cards'][0].definition
+      };
+
       console.log('next!');
+
     }
 
     this.showCard = function(index) {
